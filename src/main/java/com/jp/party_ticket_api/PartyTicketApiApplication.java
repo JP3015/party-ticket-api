c557d55d18acebbfc89080c11442cbc8ffdbf9b7
JP <jpoaraujo00@gmail.com>
2025-07-04 09:16:58 -0300

    Initial commit

---
+++ src/main/java/com/jp/party_ticket_api/PartyTicketApiApplication.java
@@ -0,0 +1,13 @@
+package com.jp.party_ticket_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyTicketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyTicketApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jp/party_ticket_api/PartyTicketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.party_ticket_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyTicketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
