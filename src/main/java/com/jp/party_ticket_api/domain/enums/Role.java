b6bb9473d0e33c0b63efe053dab007963b83b91a
JP <jpoaraujo00@gmail.com>
2025-08-22 10:03:05 -0300

    feat: implementar descrição para o enum Role

--- src/main/java/com/jp/party_ticket_api/domain/enums/Role.java
+++ src/main/java/com/jp/party_ticket_api/domain/enums/Role.java
@@ -1,6 +1,16 @@
 package com.jp.party_ticket_api.domain.enums;
 
 public enum Role {
-	ROLE_USER,
-	ROLE_ADMIN
+    ROLE_USER("Usuário"),
+    ROLE_ADMIN("Administrador");
+
+    private final String descricao;
+
+    Role(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
 }
